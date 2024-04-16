528d2fc7c2e65b0497ce0898d94501dbd2a45932
Matias <mcornalo@frba.utn.edu.ar>
2024-04-16 11:04:10 -0300

    Agrego estadoPersonaje

--- src/main/java/Tp_Anual_DDS_G1/Tp_Anual_DDS_G1/estadoPersona.java
+++ src/main/java/Tp_Anual_DDS_G1/Tp_Anual_DDS_G1/estadoPersona.java
@@ -2,4 +2,10 @@ package Tp_Anual_DDS_G1.Tp_Anual_DDS_G1;
 
 public interface estadoPersona {
 	public void colaborar();
+
+	public void donar();
+
+	public Formulario completarFormulario();	
+	
+	public void donarDinero();
 }
