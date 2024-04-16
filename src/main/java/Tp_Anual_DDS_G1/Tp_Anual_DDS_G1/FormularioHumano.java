0110b7a42e78948fa87d8190178a25845b7f5e6d
Matias <mcornalo@frba.utn.edu.ar>
2024-04-16 11:05:04 -0300

    Agrego FormularioHumano

---
+++ src/main/java/Tp_Anual_DDS_G1/Tp_Anual_DDS_G1/FormularioHumano.java
@@ -0,0 +1,11 @@
+package Tp_Anual_DDS_G1.Tp_Anual_DDS_G1;
+import java.util.Date;
+
+public class FormularioHumano{
+    String nombre;
+    String apellido; 
+    String contacto;  // (correo electrónico, teléfono, WhatsApp).
+    // Opcionales.
+    Date fechaDeNacimiento;
+    String direccion; 
+}
